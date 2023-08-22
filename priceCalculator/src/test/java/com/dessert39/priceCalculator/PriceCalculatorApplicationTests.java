f1123c2bdf99ab271684c49b413d440b24a752b4
seungyeon38 <seongyeon38@gmail.com>
2023-08-22 17:41:10 +0900

    springboot 프로젝트

---
+++ priceCalculator/src/test/java/com/dessert39/priceCalculator/PriceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dessert39.priceCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
